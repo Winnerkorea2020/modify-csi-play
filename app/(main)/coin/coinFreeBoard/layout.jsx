89830f3c90617a739e422d0b3a4e9066dc954e53
Winner2020 <68554587+Winnerkorea2020@users.noreply.github.com>
2023-11-28 20:02:20 +0900

    update Design

--- app/(main)/coin/coinFreeBoard/layout.jsx
+++
@@ -1,9 +0,0 @@
-const layout = ({ children }) => {
-  return (
-    <div className="border p-4 rounded-lg bg-white shadow-sm h-full">
-      <div>{children}</div>
-    </div>
-  );
-};
-
-export default layout;
